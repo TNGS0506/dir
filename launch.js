5976d031ccdcf761f16dae0cca70eeedd4453c16
tngs0506 <gtengis18@gmail.com>
2025-09-21 00:40:03 +0800

    little update

--- launch.js
+++ launch.js
@@ -1,12 +1,17 @@
+const os = require("os");
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+
+let executablePath = "";
+if(os.platform() === "win32") executablePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+if(os.platform() === "linux") executablePath = "/usr/bin/chromium"
 
 module.exports = async () => {
   try {
     const options = {
-        executablePath: "/usr/bin/chromium",
-      //executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-      headless: true,
+        executablePath: executablePath,
+      headless: false,
       devtools: false,
       ignoreHTTPSErrors: true,
       args: [
