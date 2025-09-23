d78d822c2796faf7481cc35d55a8fdb31ed9ad5d
tengis1 <tengis@medsoft.care>
2025-09-23 18:06:57 +0800

    sda

--- launch.js
+++ launch.js
@@ -11,7 +11,7 @@ module.exports = async () => {
   try {
     const options = {
         executablePath: executablePath,
-      headless: false,
+      headless: true,
       devtools: false,
       ignoreHTTPSErrors: true,
       args: [
